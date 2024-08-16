b69fca854c6648670eb74979a02ebafc1beb5e8a
zZzwWw <348721637@qq.com>
2024-08-16 11:41:16 +0800

    blog commit

--- deploy_xz.sh
+++ deploy_xz.sh
@@ -8,9 +8,10 @@ git add -A
 git commit -m 'blog commit'
 
 # 以下修改为推送至 main 分支
-git remote add origin git@github.com:zwking613/xz_blog.git
-git pull origin main
+# git remote add origin git@github.com:zwking613/xz_blog.git
+git push -f git@github.com/zwking613/xz_blog.git main
+# git pull origin main
 
-git push origin main
+# git push origin main
 
 cd -
