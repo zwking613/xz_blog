56ffb9eb2041f771aa290deff54af7c32b85aaae
zZzwWw <348721637@qq.com>
2024-08-16 11:38:25 +0800

    blog commit

---
+++ deploy_xz.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+git init
+git add -A
+git commit -m 'blog commit'
+
+# 以下修改为推送至 main 分支
+git remote add origin git@github.com:zwking613/xz_blog.git
+git push -f origin main
+
+cd -
